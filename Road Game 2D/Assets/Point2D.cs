28c3e657d1cb9cbda937180d68cb01e4e50cca79
Jordan <jordanjacobson2@gmail.com>
2019-08-20 14:09:46 +1000

    Added basics of Bezier Curve

---
+++ Road Game 2D/Assets/Point2D.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace Application {
+
+    public class Point2D {
+
+        private float _x;
+        private float _y;
+
+
+        public Point2D(float x, float y) {
+            this._x = x;
+            this._y = y;
+        }
+
+
+
+        public float getX() {
+            return _x;
+        }
+
+        public float getY() {
+            return _y;
+        }
+
+
+        public Vector3 getPosition() {
+            return new Vector3(this.getX(), this.getY());
+        }
+
+
+
+    }
+}
---
+++ Road Game 2D/Assets/Road.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Application;
+using System;
+
+public class Road : MonoBehaviour {
+
+    Point2D[] controlPoints = { new Point2D(0, 0), new Point2D(3, 3), new Point2D(-3, 3), new Point2D(-3, -3) };
+    int segments = 32;
+
+    [SerializeField]
+    GameObject circleGizmo;
+
+    void Start() {
+        DrawControlPointGizmos();
+
+
+    }
+
+
+
+    private Point2D GetLocationOnRoad(float t) {
+
+        float multiple = (1 - t) * (1 - t) * (1 - t);
+        Point2D p0 = new Point2D(controlPoints[0].getX() * multiple, controlPoints[0].getY() * multiple);
+
+        multiple = 3 * (1 - t) * (1 - t) * t;
+        Point2D p1 = new Point2D(controlPoints[1].getX() * multiple, controlPoints[1].getY() * multiple);
+
+        multiple = 3 * (1 - t) * t * t;
+        Point2D p2 = new Point2D(controlPoints[2].getX() * multiple, controlPoints[2].getY() * multiple);
+
+        multiple = 3 * t * t * t;
+        Point2D p3 = new Point2D(controlPoints[3].getX() * multiple, controlPoints[3].getY() * multiple);
+
+
+        return new Point2D(p0.getX() + p1.getX() + p2.getX() + p3.getX(), p0.getY() + p1.getY() + p2.getY() + p3.getY());
+    }
+
+
+
+
+    private void DrawControlPointGizmos() {
+        foreach (Point2D point in controlPoints) {
+
+            GameObject circle = Instantiate(circleGizmo, point.getPosition(), Quaternion.identity, this.transform);
+            circle.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        }
+
+    }
+
+
+}
