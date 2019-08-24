2fec0085a1e1e76170425089d04601aab8338f5d
Jordan <jordanjacobson2@gmail.com>
2019-08-24 17:44:15 +1000

    Removed 2D

---
+++ Car Game 3D/Assets/Point2D.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+
+namespace Application {
+
+    public class Point2D {
+
+        private float _x;
+        private float _y;
+
+
+        public Point2D(float x, float y) {
+            this._x = x;
+            this._y = y;
+        }
+
+
+
+        public float getX() {
+            return _x;
+        }
+
+        public float getY() {
+            return _y;
+        }
+
+        public Vector2 toVector() {
+            return new Vector2(_x, _y);
+        }
+
+
+        public Vector3 getPosition() {
+            return new Vector3(this.getX(), this.getY());
+        }
+
+        public static Point2D operator +(Point2D p1, Point2D p2) {
+            return new Point2D(p1.getX() + p2.getX(), p1.getY() + p2.getY());
+        }
+        public static Point2D operator +(Point2D p1, Vector3 vec) {
+            return new Point2D(p1.getX() + vec.x, p1.getY() + vec.y);
+        }
+
+        public static Point2D operator -(Point2D p1, Point2D p2) {
+            return new Point2D(p1.getX() - p2.getX(), p1.getY() - p2.getY());
+        }
+        public static Point2D operator -(Point2D p1, Vector3 vec) {
+            return new Point2D(p1.getX() - vec.x, p1.getY() - vec.y);
+        }
+
+        public static Point2D operator *(Point2D point, float multiple) {
+            return new Point2D(point.getX() * multiple, point.getY() * multiple);
+        }
+
+        public static Point2D operator *(Point2D point, double multiple) {
+            return new Point2D((float)(point.getX() * multiple), (float)(point.getY() * multiple));
+        }
+
+        public override string ToString() {
+            return ("[x: " + _x.ToString("0.##") + ", y: " + _y.ToString("0.##") + "]");
+        }
+    }
+}
