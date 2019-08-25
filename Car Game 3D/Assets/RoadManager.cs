3102714100f3459bf8560517500f8eb902b204f5
Jordan <jordanjacobson2@gmail.com>
2019-08-26 09:28:07 +1000

    Added instance file

---
+++ Car Game 3D/Assets/RoadManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour {
+
+    public static RoadManager instance;
+
+    private List<GameObject> roads = new List<GameObject>();
+
+    void Awake() {
+        instance = this;
+    }
+
+    public void CreateNewRoad() {
+
+    }
+
+}
