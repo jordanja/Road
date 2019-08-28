1a869102f0c14bdc6824f68b27f293828d9257f3
Jordan <jordanjacobson2@gmail.com>
2019-08-28 11:24:17 +1000

    Refactor Works!!

--- Car Game 3D/Assets/RoadManager.cs
+++ Car Game 3D/Assets/RoadManager.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Application;
 
 public class RoadManager : MonoBehaviour {
 
     public static RoadManager instance;
 
     private List<GameObject> roads = new List<GameObject>();
+
+    Point3D[] pointsOnRoad = { new Point3D(0, 0, 0), new Point3D(0, 0, 2), new Point3D(0, 0, 4), new Point3D(0, 0, 6), new Point3D(0, 0, 8) };
+
 
     [SerializeField]
     GameObject roadBlueprint;
@@ -15,9 +19,26 @@ public class RoadManager : MonoBehaviour {
         instance = this;
     }
 
-    public void CreateNewRoad() {
+    private void Start() {
+        for (int i = 0; i < pointsOnRoad.Length - 1; i++) {
+            CreateNewRoad(pointsOnRoad[i], pointsOnRoad[i + 1]);
+        }
+    }
+
+    public void CreateNewRoad(Point3D firstPoint, Point3D lastPoint) {
         GameObject road = Instantiate(roadBlueprint, transform, false);
-        road.GetComponent<Road>()?.Init();
+        if (roads.Count == 0) {
+            road.GetComponent<Road>()?.Init(firstPoint, lastPoint);
+        } else {
+            GameObject lastRoad = roads[roads.Count - 1];
+            Point3D[] controlPoints = lastRoad.GetComponent<Road>().GetControlPoints();
+            int controlPointsPerCurve = lastRoad.GetComponent<Road>().GetControlPointsPerCurve();
+            Vector3 lastVector = (controlPoints[controlPointsPerCurve - 1] - controlPoints[controlPointsPerCurve - 2]).toVector();
+
+            road.GetComponent<Road>()?.Init(firstPoint, lastPoint);
+
+        }
+        roads.Add(road);
     }
 
 }
