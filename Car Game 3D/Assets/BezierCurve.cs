71acf6ba65f5133448cd7dd2d8af1f3aa9967d9b
Jordan <jordanjacobson2@gmail.com>
2019-08-28 14:22:48 +1000

    Made Bezier independent of road

---
+++ Car Game 3D/Assets/BezierCurve.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Application;
+using System;
+
+public static class BezierCurve {
+
+
+
+    public static Point3D GetLocationOnRoad(float t, Point3D[] controlPoints) {
+
+        int numControlPoints = controlPoints.Length;
+
+        Point3D sum = new Point3D(0, 0, 0);
+
+        for (int i = 0; i < numControlPoints; i++) {
+            int binCof = BinomialCoefficient(i, numControlPoints - 1);
+
+            double term = binCof * Math.Pow((1 - t), (numControlPoints - 1 - i)) * Math.Pow(t, i);
+
+
+            sum = sum + (controlPoints[i] * term);
+
+        }
+
+        return sum;
+
+
+    }
+
+
+    public static Vector3 GetDerivitiveOnRoad(float t, Point3D[] controlPoints) {
+
+        int numControlPoints = controlPoints.Length;
+        Point3D sum = new Point3D(0, 0, 0);
+
+        for (int i = 0; i < numControlPoints - 1; i++) {
+            int binCof = BinomialCoefficient(i, numControlPoints - 2);
+
+            double term = binCof * Math.Pow(t, i) * Math.Pow((1 - t), (numControlPoints - 2 - i));
+
+            sum = sum + ((controlPoints[i + 1] - controlPoints[i]) * term);
+
+        }
+        return sum.toVector().normalized;
+    }
+
+
+    private static int BinomialCoefficient(int k, int n) {
+
+        int result = 1;
+        for (int i = 1; i <= k; i++) {
+            result *= n - (k - i);
+            result /= i;
+        }
+        return result;
+
+    }
+
+
+
+
+
+
+}
--- Car Game 3D/Assets/Road.cs
+++ Car Game 3D/Assets/Road.cs
@@ -93,9 +93,9 @@ public class Road : MonoBehaviour {
 
     }
 
-    //internal void GetPositionOnRoad(float fractionAlongCurrentRoad) {
-        
-    //}
+    internal Point3D GetLocationOnRoad(float fractionAlongCurrentRoad) {
+        return BezierCurve.GetLocationOnRoad(fractionAlongCurrentRoad, controlPoints);
+    }
 
     private float GetRandomOffset() {
         return roadCurviness * UnityEngine.Random.Range(-1f, +1f);
@@ -108,7 +108,7 @@ public class Road : MonoBehaviour {
         Point3D[] fractionalPointsAlongBezier = new Point3D[segments + 1];
         for (int i = 0; i < segments + 1; i++) {
             float percentageThroughRoad = (float)(((float)i) / ((float)segments));
-            fractionalPointsAlongBezier[i] = GetLocationOnRoad(percentageThroughRoad);
+            fractionalPointsAlongBezier[i] = BezierCurve.GetLocationOnRoad(percentageThroughRoad, controlPoints);
         }
 
 
@@ -128,7 +128,7 @@ public class Road : MonoBehaviour {
 
         for (int i = 0; i < segments + 1; i++) {
             float percentageThroughRoad = (float)(((float)i) / ((float)segments));
-            Vector3 derivitive = GetDerivitiveOnRoad(percentageThroughRoad);
+            Vector3 derivitive = BezierCurve.GetDerivitiveOnRoad(percentageThroughRoad, controlPoints);
 
             Vector3 rightAngle = RightAngleVector(derivitive) * roadWidth;
 
@@ -183,55 +183,6 @@ public class Road : MonoBehaviour {
         return rowVector;
     }
 
-    public Point3D GetLocationOnRoad(float t) {
-
-        int numControlPoints = controlPoints.Length;
-
-        Point3D sum = new Point3D(0, 0, 0);
-
-        for (int i = 0; i < numControlPoints; i++) {
-            int binCof = BinomialCoefficient(i, numControlPoints - 1);
-
-            double term = binCof * Math.Pow((1 - t), (numControlPoints - 1 - i)) * Math.Pow(t, i);
-
-
-            sum = sum + (controlPoints[i] * term);
-
-        }
-
-        return sum;
-
-
-    }
-
-
-    private Vector3 GetDerivitiveOnRoad(float t) {
-
-        int numControlPoints = controlPoints.Length;
-        Point3D sum = new Point3D(0, 0, 0);
-
-        for (int i = 0; i < numControlPoints - 1; i++) {
-            int binCof = BinomialCoefficient(i, numControlPoints - 2);
-
-            double term = binCof * Math.Pow(t, i) * Math.Pow((1 - t), (numControlPoints - 2 - i));
-
-            sum = sum + ((controlPoints[i + 1] - controlPoints[i]) * term);
-
-        }
-        return sum.toVector().normalized;
-    }
-
-
-    private int BinomialCoefficient(int k, int n) {
-
-        int result = 1;
-        for (int i = 1; i <= k; i++) {
-            result *= n - (k - i);
-            result /= i;
-        }
-        return result;
-
-    }
 
     private void DrawControlPointGizmos() {
 
