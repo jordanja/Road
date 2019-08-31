3e398121b7e6b520846461a71d3d04329a84b2b6
Jordan <jordanjacobson2@gmail.com>
2019-08-31 15:20:12 +1000

    3rd Person Camera works

---
+++ Car Game 3D/Assets/FollowCar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCar : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform Car;
+
+    Vector3 translationOffset = new Vector3(0,1,-2);
+    Vector3 rotationOffset = new Vector3(30,0,0);
+
+
+    void LateUpdate() {
+        
+        transform.position = Car.position;
+        transform.rotation = Car.rotation;
+
+        transform.Translate(translationOffset);
+        transform.Rotate(rotationOffset);
+    }
+}
