3daa40f4a30dd167cbe7e3321df1d6871769f5de
Jordan <jordanjacobson2@gmail.com>
2019-08-31 14:34:58 +1000

    Adding third person camera

---
+++ Car Game 3D/Assets/CameraManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+    [SerializeField]
+    Camera GodCamera;
+
+    [SerializeField]
+    Camera ThirdPersonCamera;
+
+    public void ChooseGodCamera() {
+        GodCamera.enabled = true;
+        ThirdPersonCamera.enabled = false;
+    }
+
+    public void ChooseThirdPersonCamera() {
+        ThirdPersonCamera.enabled = true;
+        GodCamera.enabled = false;
+    }
+
+
+}
