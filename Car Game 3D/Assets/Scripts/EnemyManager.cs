edabad13953b80215f85d58b67f8ea711dcb224f
Jordan <jordanjacobson2@gmail.com>
2019-09-06 09:44:29 +1000

    Enemy refactoring

--- Car Game 3D/Assets/Scripts/EnemyManager.cs
+++ Car Game 3D/Assets/Scripts/EnemyManager.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
     public static EnemyManager instance;
+
+    [SerializeField]
+    GameObject _enemy;
 
     void Awake() {
         instance = this;
@@ -16,9 +20,12 @@ public class EnemyManager : MonoBehaviour
 
     IEnumerator SendEnemies() {
         while(true) {
-
+            sendEnemy();
             yield return new WaitForSeconds(3f);
         }
     }
 
+    void sendEnemy() {
+        GameObject enemy = Instantiate(_enemy);
+    }
 }
