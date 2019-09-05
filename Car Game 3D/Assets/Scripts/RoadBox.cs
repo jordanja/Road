b4ff3c84446993b02b0fcc40d162caa816e9ec46
Jordan <jordanjacobson2@gmail.com>
2019-09-05 12:28:25 +1000

    Added texture to walls

--- Car Game 3D/Assets/Scripts/RoadBox.cs
+++ Car Game 3D/Assets/Scripts/RoadBox.cs
@@ -38,6 +38,7 @@ public class RoadBox : MonoBehaviour {
         newMesh.Clear();
 
         Vector3[] vertices = new Vector3[10 * (baseRoadMesh.vertices.Length / 2)];
+        Vector2[] uv = new Vector2[10 * (baseRoadMesh.vertices.Length / 2)];
 
         Vector3 loweredVertice = new Vector3(0, 0.2f, 0);
 
@@ -55,11 +56,17 @@ public class RoadBox : MonoBehaviour {
             vertices[i * 5 + 3] = vertexLocation;
             vertices[i * 5 + 4] = vertexLocation + new Vector3(0, -depthBelowRoad, 0);
 
+            uv[i * 5 + 0] = new Vector2(1f * 0.2f, i/(float)baseRoadMesh.vertices.Length);
+            uv[i * 5 + 1] = new Vector2(1f * 0.4f, i/(float)baseRoadMesh.vertices.Length);
+            uv[i * 5 + 2] = new Vector2(1f * 0.6f, i/(float)baseRoadMesh.vertices.Length);
+            uv[i * 5 + 3] = new Vector2(1f * 0.8f, i/(float)baseRoadMesh.vertices.Length);
+            uv[i * 5 + 4] = new Vector2(1f * 1f,   i/(float)baseRoadMesh.vertices.Length);
+
 
         }
         // print(vertices.Length);
         newMesh.vertices = vertices;
-
+        newMesh.uv = uv;
         int[] triangles = generateTriangles(baseRoadMesh);
 
         newMesh.triangles = triangles;
