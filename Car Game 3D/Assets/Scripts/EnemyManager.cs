9cdaef20b190f2c692713131a144cab6fb6e8c97
Jordan <jordanjacobson2@gmail.com>
2019-09-06 12:07:15 +1000

    This works

--- Car Game 3D/Assets/Scripts/EnemyManager.cs
+++ Car Game 3D/Assets/Scripts/EnemyManager.cs
@@ -10,18 +10,22 @@ public class EnemyManager : MonoBehaviour
     [SerializeField]
     GameObject _enemy;
 
+
     void Awake() {
         instance = this;
     }
 
     void Start() {
         Coroutine sendEnemy = StartCoroutine(SendEnemies());
+
     }
+
 
     IEnumerator SendEnemies() {
         while(true) {
-            sendEnemy();
-            yield return new WaitForSeconds(3f);
+                sendEnemy();
+                yield return new WaitForSeconds(3f);
+            
         }
     }
 
