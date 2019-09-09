bd6ebb89aa897cb472be8bb93ed0e1e9f1fd2c68
Jordan <jordanjacobson2@gmail.com>
2019-09-09 13:20:09 +1000

    Adding landscape generator

---
+++ Car Game 3D/Assets/Scripts/LandscapeManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandscapeManager : MonoBehaviour {
+    
+    public static LandscapeManager instance;
+
+    [SerializeField]
+    GameObject LandscapeBlueprint;
+
+    List<GameObject> landscapes;
+
+    void Awake() {
+        instance = this;
+    }
+
+    void Start() {
+        landscapes = new List<GameObject>();
+    }
+
+    public void CreateLandscapeBetweenZPoints(float z1, float z2) {
+        GameObject landscapeParent = new GameObject();
+        landscapeParent.name = "Landscape Parent " + landscapes.Count;
+        landscapeParent.transform.parent = transform.parent;
+
+        GameObject landscape = Instantiate(LandscapeBlueprint, landscapeParent.transform, false);
+        landscape.name = "Landscape " + landscapes.Count;
+
+
+    }
+
+
+}
