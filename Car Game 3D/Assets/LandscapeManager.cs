fb8f8fa1ad7620f8c529ccd3161b57622a17c207
Jordan <jordanjacobson2@gmail.com>
2019-09-09 11:42:09 +1000

    Added landscape manager

---
+++ Car Game 3D/Assets/LandscapeManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandscapeManager : MonoBehaviour {
+    
+    public LandscapeManager instance;
+
+    void Awake() {
+        instance = this;
+    }
+
+    public void CreateLandscapeBetweenZPoints(float z1, float z2) {
+        Mesh landscapeMesh = new Mesh();
+        
+
+
+
+    }
+
+
+}
