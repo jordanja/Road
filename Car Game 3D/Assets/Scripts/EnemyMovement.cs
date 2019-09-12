dda99ad29d3f2640b530d83da9dea1bda5a4826d
Jordan <jordanjacobson2@gmail.com>
2019-09-12 15:32:48 +1000

    Adding mountain pooling system

--- Car Game 3D/Assets/Scripts/EnemyMovement.cs
+++ Car Game 3D/Assets/Scripts/EnemyMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Profiling;
 
 public class EnemyMovement : MonoBehaviour {
 
---
+++ Car Game 3D/Assets/Scripts/MountainsPool.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountainsPool : MonoBehaviour {
+    
+    [SerializeField]
+    private GameObject mountainPrefab;
+
+    private Queue<GameObject> mountainQueue = new Queue<GameObject>();
+
+    public static MountainsPool instance {get; private set;}
+
+    int numInstantiated = 0;
+
+    GameObject MountainParent;
+
+    private void Awake() {
+        instance = this;
+        
+        MountainParent = new GameObject();
+        MountainParent.name = "Mountain Parent";
+        MountainParent.transform.parent = transform;
+        
+        AddMountains(10);
+    }
+
+    void Start() {
+        
+    }
+
+    public GameObject Get() {
+        if (mountainQueue.Count == 0) {
+            AddMountains(1);
+        }
+        GameObject obj = mountainQueue.Dequeue();
+        mountainQueue.Enqueue(obj);
+        return obj;
+    }
+
+
+    private void AddMountains(int count) {
+        for (int i = 0; i < count; i++) {
+            GameObject mountainToInstantiate = Instantiate(mountainPrefab);
+            mountainToInstantiate.transform.parent = MountainParent.transform;
+            mountainToInstantiate.SetActive(false);
+            mountainQueue.Enqueue(mountainToInstantiate);
+            numInstantiated++;
+        }
+    }
+
+    public void ReturnToPool(GameObject obj) {
+        obj.SetActive(false);
+    }
+
+    
+}
