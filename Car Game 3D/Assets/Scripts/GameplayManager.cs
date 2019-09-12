bb58d52b74a1d165579a646bb73687982e63d1fe
Jordan <jordanjacobson2@gmail.com>
2019-09-12 11:36:38 +1000

    Highscore

--- Car Game 3D/Assets/Scripts/GameplayManager.cs
+++ Car Game 3D/Assets/Scripts/GameplayManager.cs
@@ -12,14 +12,22 @@ public class GameplayManager : MonoBehaviour {
     [SerializeField]
     TextMeshProUGUI ScoreText;
 
-    float currentScore;
+    [SerializeField]
+    TextMeshProUGUI HighScoreText;
+
+    int currentScore;
 
     string defaultScoreText = "Score: ";
+    string defaultHighScoreText = "Highscore: ";
+
+    string highScoreKey = "HighScore";
 
     void Awake() {
         instance = this;
         currentScore = 0;
         updateScoreText();
+        updateHightScoreText();
+
     }
 
     public void IncreaseScore() {
@@ -27,11 +35,20 @@ public class GameplayManager : MonoBehaviour {
         updateScoreText();
     }
 
+    private void updateHightScoreText() {
+        HighScoreText.text = defaultHighScoreText + PlayerPrefs.GetInt(highScoreKey, 0).ToString();
+    }
+
     private void updateScoreText() {
         ScoreText.text = defaultScoreText + currentScore.ToString();
     }
 
     public void RestartGame() {
+        if (currentScore > PlayerPrefs.GetInt(highScoreKey, 0)) {
+            PlayerPrefs.SetInt(highScoreKey,currentScore);
+            updateHightScoreText();
+            
+        }
         SceneManager.LoadScene(0);
     }
 
