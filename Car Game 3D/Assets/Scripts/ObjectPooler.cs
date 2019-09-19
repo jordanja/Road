6aada29f50eeed56e589d0cd2986e0a14f83c9d6
Jordan <jordanjacobson2@gmail.com>
2019-09-19 17:07:51 +1000

    Fixing pooling system

---
+++ Car Game 3D/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour {
+
+    [System.Serializable]
+    public class Pool {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public static ObjectPooler instance;
+
+    void Awake() {
+        instance = this;
+    }
+
+    void Start() {
+        
+    }
+
+
+}
